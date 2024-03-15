e4cdba20663bcea43fb93787bd09ee86bb1254d7
zapboy216 <zapboy216@gmail.com>
2024-03-15 08:19:32 -0400

    Add MyUrlField component to UserList

--- src/users.tsx
+++ src/users.tsx
@@ -1,5 +1,6 @@
 import { useMediaQuery, Theme } from "@mui/material";
 import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
+import MyUrlField from './MyUrlField';
 
 export const UserList = () => {
     const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
@@ -15,11 +16,11 @@ export const UserList = () => {
                 <Datagrid rowClick="edit">
                     <TextField source="id" />
                     <TextField source="name" />
-                    <TextField source="username" />
+                 
                     <EmailField source="email" />
-                    <TextField source="address.street" />
+                   
                     <TextField source="phone" />
-                    <TextField source="website" />
+                    <UrlField source="website" />
                     <TextField source="company.name" />
                 </Datagrid>
             )}
