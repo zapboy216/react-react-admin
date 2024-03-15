349da8f5ed0ede7844b9903733da77ee7f66c879
zapboy216 <zapboy216@gmail.com>
2024-03-15 10:22:00 -0400

    Add authProvider to Admin component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { dataProvider } from './dataProvider';
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { Dashboard } from './Dashboard';
+import { authProvider } from './authProvider';
 
 export const App = () => (
-  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard}>
 
 <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
    <Resource name="users" list={UserList} show={ShowGuesser} recordRepresentation="name" />
