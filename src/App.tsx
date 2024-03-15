801dfcc74f95d8452658519bc0f58c069db37d39
zapboy216 <zapboy216@gmail.com>
2024-03-15 10:11:16 -0400

    Add Dashboard component to Admin component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { Admin, Resource, ShowGuesser } from 'react-admin';
 import { dataProvider } from './dataProvider';
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
+import { Dashboard } from './Dashboard';
 
 export const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
 
 <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
    <Resource name="users" list={UserList} show={ShowGuesser} recordRepresentation="name" />
