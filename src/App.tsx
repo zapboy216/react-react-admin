1d93791d897c6b8ffeb4b0a5c69abde94657e250
zapboy216 <zapboy216@gmail.com>
2024-03-15 09:15:01 -0400

    Add Edit functionality to Post resource

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Admin, Resource, ShowGuesser } from 'react-admin';
+import { Admin, Resource, ShowGuesser, EditGuesser } from 'react-admin';
 import { dataProvider } from './dataProvider';
 import { UserList } from "./users";
 import { PostList } from "./posts";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />
+    <Resource name="posts" list={PostList} edit={EditGuesser} />
    <Resource name="users" list={UserList} show={ShowGuesser} recordRepresentation="name" />
   </Admin>
 );
--- src/posts.tsx
+++ src/posts.tsx
@@ -1,10 +1,5 @@
-import {
-  Datagrid,
-  List,
-  ReferenceField,
-  TextField,
-  EditButton,
-} from "react-admin";
+import { Datagrid, List, ReferenceField, TextField, EditButton,  Edit, ReferenceInput, SimpleForm, TextInput } from "react-admin";
+
 
 export const PostList = () => (
   <List>
@@ -16,4 +11,16 @@ export const PostList = () => (
       <EditButton />
     </Datagrid>
   </List>
+);
+
+
+export const PostEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="id" />
+            <TextInput source="title" />
+            <TextInput source="body" />
+        </SimpleForm>
+    </Edit>
 );
